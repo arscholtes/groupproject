fb7ca18717c46618270a53d5594b41395cdafe62
Jason Tuttle <jason@hearspotbark.com>
2017-06-06 13:31:23 -0400

    commit madlib function

--- utils.js
+++ utils.js
@@ -1,1 +1,8 @@
-
+function madlib(company, offering, audience, problem, secret) {
+  let message = "My company, " + company;
+  message += ", is developing " + offering;
+  message += " to help " + audience;
+  message += " solve the problem of " + problem;
+  message += " with " + secret + ".";
+  return message;
+}
