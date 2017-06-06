a18c3e91aa5239b27c0242538a89a9009c6f4b62
Alex Scholtes <alex@ascholtes.me>
2017-06-06 14:35:36 -0400

    Update utils.js
    last bit of code

--- utils.js
+++ utils.js
@@ -67,3 +67,15 @@ function greaterThan (p1,p2) {
   return p2 > p1;
 }
 
+
+//vowels true or false//
+
+function vowel (x) {
+  if (x == "a" || x =="e" || x =="i" || x == "o" || x == "u"){
+    result = true
+  }
+  else {
+  result = false;
+  }
+  return result;
+}
