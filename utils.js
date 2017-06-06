90382be2f72278a1c0dcc38d8feee5e3857958a1
Rebekkah Porter <rebekkah1@me.com>
2017-06-06 14:34:02 -0400

    submit function 3

--- utils.js
+++ utils.js
@@ -1,1 +1,11 @@
+//vowels true or false//
 
+function vowel (x) {
+  if (x == "a" || x =="e" || x =="i" || x == "o" || x == "u"){
+    result = true
+  }
+  else {
+  result = false;
+  }
+  return result;
+}
